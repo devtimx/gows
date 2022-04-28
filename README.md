5463e4cb5a9172318a3a389465bca5c5cb2f672f
manuelmannuel <manuelmannuel@gmail.com>
2022-04-28 15:11:35 -0500

    implement GoQuery

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # gows
 
-In process ...
+### Simple scraper for amazon 
+
+Replace the URLs in the following code block with your URLs
+
+```
+var urlList = []string{
+	//"https://www.amazon.com.mx/dp/B07Z4681LQ",
+	"https://www.amazon.com.mx/dp/B07ZQS6LPZ",
+	"https://www.amazon.com.mx/dp/B07FDF9B46",
+	"https://www.amazon.com.mx/dp/B0934C48VN",
+	"https://www.amazon.com.mx/dp/B09F5VC4SF",
+}
+
+```
--- main.go
+++ main.go
@@ -8,12 +8,14 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+/*userAgent simulate a browser*/
 const (
 	userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
 )
 
 var urlList = []string{
-	"https://www.amazon.com.mx/dp/B07Z4681LQ",
+	//"https://www.amazon.com.mx/dp/B07Z4681LQ",
+	"https://www.amazon.com.mx/dp/B07ZQS6LPZ",
 	"https://www.amazon.com.mx/dp/B07FDF9B46",
 	"https://www.amazon.com.mx/dp/B0934C48VN",
 	"https://www.amazon.com.mx/dp/B09F5VC4SF",
@@ -46,59 +48,33 @@ func main() {
 	}
 	wg.Wait()
 	for _, product := range products {
-		fmt.Println(product.Name)
+		fmt.Printf("Name: %s Price: %s OldProce: %s Brand: %s\n", product.Name, product.Price, product.OldPrice, product.Brand)
 	}
 	close(chanel)
 }
 
+/* scraperUrl resolves URL, extract data and returns a string*/
 func scraperUrl(url string, chanel chan<- Product) {
-	var product Product
+	var products Product
 	client := &http.Client{}
 	request, _ := http.NewRequest("GET", url, nil)
 	request.Header.Set("User-Agent", userAgent)
 	resp, _ := client.Do(request)
 
-	name, err := inspectHtml(resp, "#productTitle")
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
-		println(err)
 		return
 	}
-
-	/*price, err := inspectHtml(resp, "spam .a-price spam .a-offscreen ")
-	if err != nil {
-		println(err)
-		return
-	}*/
-
-	/*oldPrice, err := inspectHtml(resp, "#")
-	if err != nil {
-		println(err)
-		return
-	}*/
-
-	/*brand, err := inspectHtml(resp, ".po-brand .a-size-base")
-	if err != nil {
-		println(err)
-		return
-	}*/
-
-	product = Product{
-		Name: name,
-	}
+	doc.Each(func(i int, s *goquery.Selection) {
+		products = Product{
+			Name:     s.Find("#productTitle").Text(),
+			Price:    s.Find("#corePrice_feature_div .a-price span.a-offscreen").Text(),
+			OldPrice: s.Find("span[data-a-color='secondary'] .a-offscreen").Text(),
+			Brand:    s.Find(".po-brand .a-span9 span.a-size-base").Text(),
+		}
+	})
 
 	defer func() {
-		chanel <- product
+		chanel <- products
 	}()
-}
-
-func inspectHtml(req *http.Response, tags string) (string, error) {
-	item := ""
-	doc, err := goquery.NewDocumentFromReader(req.Body)
-	if err != nil {
-		return "", err
-	}
-	doc.Find(tags).Each(func(i int, s *goquery.Selection) {
-		item = s.Text()
-	})
-	return item, nil
 }
